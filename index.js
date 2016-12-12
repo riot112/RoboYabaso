8712a31d610c7ea6a8a5ea75bd9fab167d1b5118
zeteticl <cougar.l@hotmail.com>
2016-12-12 22:38:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -482,16 +482,19 @@ function wod(triggermsg ,text) {
 	var returnStr = triggermsg+' [';
 	var wodarray = [];
 	var varcou = 0;
-    var varsu = 0;
+	var varsu = 0;
 	var match = /^(\d+)(wd|wod)(\d|)((\+|-)(\d+)|)$/i.exec(triggermsg);   //判斷式  [0]3wd8+10,[1]3,[2]wd,[3]8,[4]+10,[5]+,[6]10  
-if (match[3]<="1") 
-{ 
+	if (match[3] <= 1) 
+
+	{ 
 	return '加骰需要比1大; 
-}
-if (match[3]=="") { 
-	match[3] =10; 
-}
-for (var i = 0; i < Number(match[1]); i++)	
+	}
+
+	if (match[3] =="") { 
+		match[3] =10; 
+	}
+
+	for (var i = 0; i < Number(match[1]); i++)	
 	{
              varcou =  Math.floor(Math.random() * 10) + 1;
              returnStr += varcou +', ';
