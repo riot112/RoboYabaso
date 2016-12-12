30acc84b6504a32ae85ecfb80493bc6198a89bbe
zeteticl <cougar.l@hotmail.com>
2016-12-13 00:01:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -479,28 +479,43 @@ function nechronica(triggermsg ,text) {
 ////////////////////////////////////////
 
 function wod(triggermsg ,text) {
-	var returnStr = triggermsg+' [';
-	var wodarray = [];
+	var triggermsg = '3wd8';
+var returnStr = triggermsg + ' [';
 	var varcou = 0;
 	var varsu = 0;
+    var text = "";
 	var match = /^(\d+)(wd|wod)(\d|)((\+|-)(\d+)|)$/i.exec(triggermsg);   //判斷式  [0]3wd8+10,[1]3,[2]wd,[3]8,[4]+10,[5]+,[6]10  
-
-			
+	if (match[3]<=1) 
+    { 
+    return = '加骰最少比1高'; 
+    }
 for (var i = 0; i < Number(match[1]); i++)	
 	{
              varcou =  Math.floor(Math.random() * 10) + 1;
              returnStr += varcou +', ';
-             if (match[3]=="") { match[3] =10 }
-		if (match[3]<=1) { return '加骰最少比1高'; }
-             if (varcou >=match[3]) { i--}
+             if (match[3]=="") { 
+             match[3] =10 
+             }
+             if (varcou >=match[3]) 
+             { i--;
+             }
              if (varcou >=8) 
 	     {
 		     varsu++;
 	     }
 
 	}
+    if(match[5]=='+'){
+    
     for (var i = 0; i < Number(match[6]); i++)	{
 	    varsu++;
+    }
+    }
+    if(match[5]=='-'){
+    
+    for (var i = 0; i < Number(match[6]); i++)	{
+	    varsu--;
+    }
     }
     returnStr = returnStr.replace(/[,][ ]$/,'] → '+varsu+'成功');
 	if (text != null){
