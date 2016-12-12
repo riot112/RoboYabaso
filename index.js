a944e5628b0eb5dfa7afc4e1cc24f9df3fe42adc
zeteticl <cougar.l@hotmail.com>
2016-12-12 22:57:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -485,18 +485,15 @@ function wod(triggermsg ,text) {
 	var varsu = 0;
 	var match = /^(\d+)(wd|wod)(\d|)((\+|-)(\d+)|)$/i.exec(triggermsg);   //判斷式  [0]3wd8+10,[1]3,[2]wd,[3]8,[4]+10,[5]+,[6]10  
 
-
-	if (match[3] =="") { 
-		match[3] =10; 
-	}
-
-	for (var i = 0; i < Number(match[1]); i++)	
+			
+for (var i = 0; i < Number(match[1]); i++)	
 	{
              varcou =  Math.floor(Math.random() * 10) + 1;
              returnStr += varcou +', ';
-             if (match[3]=="") { match[3] =10 ;}
-             if (varcou >=match[3]) { i--;}
-             if (varcou <=4) return  null;
+             if (match[3]=="") { match[3] =10 }
+		if (match[3]<=1) { return '加骰最少比1高'; }
+             if (varcou >=match[3]) { i--}
+             if (varcou >=8) 
 	     {
 		     varsu++;
 	     }
